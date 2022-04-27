a0ce0a2825ec4609cfbcf92697f486dfd6c8c0b3
unknown <rafahmp123@gmail.com>
2022-04-27 11:04:05 -0300

    Adjusting email check script

--- challenges/Grab/script.js
+++ challenges/Grab/script.js
@@ -2,6 +2,7 @@ const email = document.getElementById("email");
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const password = document.getElementById("password");
 const loginBtn = document.querySelector(".btn");
+const errorMsg = document.querySelector(".error-txt");
 
 function checkBtn() {
   loginBtn.addEventListener("click", () => {
@@ -13,12 +14,12 @@ function checkEmail(email) {
   this.email.classList.remove('active');
   if(!emailRegex.test(email.value)) {
     this.email.classList.add('active');
-    console.log(this.email);
+    errorMsg.classList.add('active');
   } else {
     this.email.classList.remove('active');
+    errorMsg.classList.remove('active');
   }
 }
 
-console.log(this.email);
 
 checkBtn();
